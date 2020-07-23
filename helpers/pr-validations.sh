a2cab9483020503ec68f08222461795913d1597c
prathapreddy <parvathareddy@google.com>
2020-07-23 16:25:47 -0700

    Added rebase checks

--- helpers/pr-validations.sh
+++ helpers/pr-validations.sh
@@ -11,3 +11,14 @@ echo "HEAD_BRANCH=${HEAD_BRANCH}"
 echo "BASE_BRANCH=${BASE_BRANCH}"
 echo "HEAD_REPO_URL=${HEAD_REPO_URL}"
 echo "PR_NUMBER=${PR_NUMBER}"
+
+git clone "${BASE_REPO_URL}"
+cd "${REPO_NAME}"
+git fetch origin refs/pull/${PR_NUMBER}/head:validate#${PR_NUMBER}
+git checkout validate#${PR_NUMBER}
+if ! git rebase "origin/${BASE_BRANCH}"
+then
+  exit 1
+fi
+
+echo "PR${PR_NUMBER} can be rebased successfully on ${BASE_BRANCH} branch."
