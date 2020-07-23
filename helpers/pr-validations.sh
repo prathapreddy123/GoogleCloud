b9573040b0fdb88dac0d46362734d01e7de3883c
prathapreddy <parvathareddy@google.com>
2020-07-23 13:11:24 -0700

    Included Cloud Build file

---
+++ helpers/pr-validations.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo "** Printing all variables **"
+
+echo "PROJECT_ID=${PROJECT_ID}"
+echo "COMMIT_SHA=${COMMIT_SHA}"
+echo "SHORT_SHA=${SHORT_SHA}"
+echo "REPO_NAME=${REPO_NAME}"
+echo "BRANCH_NAME=${BRANCH_NAME}"
+echo "HEAD_BRANCH=${HEAD_BRANCH}"
+echo "BASE_BRANCH=${BASE_BRANCH}"
+echo "HEAD_REPO_URL=${HEAD_REPO_URL}"
+echo "PR_NUMBER=${PR_NUMBER}"
