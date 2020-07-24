5cb96d83ab0fe7483323726951df1c04b7d5d15a
prathapreddy <parvathareddy@google.com>
2020-07-23 22:13:59 -0700

    Back to rebase

--- helpers/pr-validations.sh
+++ helpers/pr-validations.sh
@@ -15,13 +15,13 @@ echo "PR_NUMBER=${PR_NUMBER}"
 git clone "${BASE_REPO_URL}"
 echo "Repo cloned successfully"
 cd "${REPO_NAME}"
-#git config user.email "presubmit@example.com"
-#git config user.name "presubmit"
+git config user.email "presubmit@example.com"
+git config user.name "presubmit"
 git fetch origin refs/pull/${PR_NUMBER}/head:validate#${PR_NUMBER}
 echo "pull ref created"
 git checkout validate#${PR_NUMBER}
 #merge --ff-only rebase
-if ! git merge --ff-only "origin/${BASE_BRANCH}"
+if ! git rebase "origin/${BASE_BRANCH}"
 then
   echo "PR#${PR_NUMBER} cannot be rebased automatically. Resolve conflicts manually"
   exit 1
